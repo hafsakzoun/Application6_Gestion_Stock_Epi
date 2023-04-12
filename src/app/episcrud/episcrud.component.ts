7704460c2000278881696ac7b4834c33bfd60210
hafsakzoun <118023551+hafsakzoun@users.noreply.github.com>
2023-04-12 15:11:41 +0000

    hafsa2

--- src/app/episcrud/episcrud.component.ts
+++ src/app/episcrud/episcrud.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EpiscrudComponent implements OnInit  {
 
-  EmployeeArray : any[] = [];
+  EpisArray : any[] = [];
   isResultLoaded = false;
   isUpdateFormActive = false;
  
@@ -22,14 +22,119 @@ export class EpiscrudComponent implements OnInit  {
   quantity: number=0;
  
   currentEpiID = "";
+
   constructor(private http: HttpClient )
   {
-
+    this.getAllEpi();
 
   }
   ngOnInit(): void{
 
   }
+
+  getAllEpi()
+  {
+    
+    this.http.get("http://127.0.0.1:8000/api/epis")
+  
+    .subscribe((resultData: any)=>
+    {
+        this.isResultLoaded = true;
+        console.log(resultData);
+        this.EpisArray = resultData;
+    });
+  }
+  register()
+  {
+    let bodyData={
+      'label': this.label,
+      'category': this.category,
+      'type': this.type,
+      'size': this.size,
+      'description': this.description,
+      'status': this.status,
+      'quantity': this.quantity,
+    };
+   
+    this.http.post("http://127.0.0.1:8000/api/save",bodyData).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Epi Registered Successfully")
+        this.getAllEpi();
+        this.label = '';
+        this.category = '';
+        this.type= '';
+        this.size='';
+        this.description='';
+        this.status='';
+        this.quantity=0;
+    });
+  }
+  
+  setUpdate(data: any)
+  {
+   this.label = data.label;
+   this.category = data.category;
+   this.type = data.type;
+   this.size = data.size;
+   this.description = data.description;
+   this.status = data.status;
+   this.quantity= data.quantity;
+   this.currentEpiID = data.id;
+  }
+
+  UpdateRecords()
+  {
+    let bodyData = {
+      "label" : this.label,
+      "category" : this.category,
+      "type" : this.type,
+      "size" : this.size,
+      "description" :this.description,
+      "status" : this.status,
+      "quantity" : this.quantity,
+    };
+
+    this.http.put("http://127.0.0.1:8000/api/update"+ "/"+ this.currentEpiID,bodyData).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Employee Registered Updateddd")
+        this.getAllEpi();
+        this.label = '';
+        this.category = '';
+        this.type='';
+        this.size='';
+        this.description='';
+        this.status='';
+        this.quantity  = 0;
+    });
+  }
+
+  save()
+  {
+    if(this.currentEpiID == '')
+    {
+        this.register();
+    }
+      else
+      {
+       this.UpdateRecords();
+      }      
  
+  }
+
+  setDelete(data: any)
+  {
+    
+    
+    this.http.delete("http://127.0.0.1:8000/api/delete"+ "/"+ data.id).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Epi Deleted")
+        this.getAllEpi();
+  
+    });
+ 
+  }
 
 }
