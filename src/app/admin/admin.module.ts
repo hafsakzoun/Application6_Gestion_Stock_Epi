f532b87d223001bac4d29c665b1c2fd7d756fe76
hafsakzoun <118023551+hafsakzoun@users.noreply.github.com>
2023-04-26 11:39:10 +0000

    hero

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,7 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-
+import { RouterModule } from '@angular/router'; 
 
 
 @NgModule({
@@ -20,6 +20,7 @@ import { MatMenuModule } from '@angular/material/menu';
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
+    RouterModule,
   ]
 })
 export class AdminModule { }
