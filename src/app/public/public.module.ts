7d98be2853a7af5d11ee4898cf92dc8dbd0ae55d
hafsakzoun <118023551+hafsakzoun@users.noreply.github.com>
2023-04-26 10:21:00 +0000

    hafsa4/26

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicComponent } from './public.component';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -9,7 +8,6 @@ import { SignupComponent } from './signup/signup.component';
 @NgModule({
   declarations: [
     PublicComponent,
-    HomeComponent,
     LoginComponent,
     SignupComponent
   ],
